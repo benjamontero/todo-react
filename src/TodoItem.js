15a8e7ddf0b5de782c11837b03a05b24cec9c5a4
benjamontero <benjamontero@gmail.com>
2020-01-08 22:35:08 -0300

    add dynamic list of TaskItems

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -4,7 +4,7 @@ import React, { Component } from "react";
 export default class TodoItem extends Component {
     render(){
         return(
-        <p> item</p>
+        <p> {this.props.task}</p>
         )
     }
 }
